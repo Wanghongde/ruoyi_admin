b21bace4118ac9ff2795a9a708cf9dc9faa5533c
王洪德 <315020798@qq.com>
2023-05-18 17:30:45 +0800

    tagsView样式

---
+++ src/layout/components/TagsView/scrollPane.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-scrollbar class="scroll-container">
+    <slot></slot>
+  </el-scrollbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.scroll-container{
+  width: 100%;
+}
+</style>
